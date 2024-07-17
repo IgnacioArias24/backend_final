5936890a27a755883e69fd1b5d8fa0749a80ec37
IgnacioArias24 <piru@MacBook-Air-de-Ine.local>
2024-07-17 13:10:15 +0200

    prueba

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get('/test', (req,res)=>{
+    res.json({status: 200, message: 'Hello World'})
+} )
+
+
 app.use('/api/auth', authRouter)
 app.use('/api/products', productRouter)
 app.use('/api/carts', cartsRouter)
